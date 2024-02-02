60b83fb9f131d2482b3c253647e1757706ff44a5
auishi22 <auishi229@gmail.com>
2024-02-02 22:27:18 +0600

    task 6 done

--- task06.js
+++ task06.js
@@ -9,6 +9,7 @@ else if (age >= 10 && age <= 30) {
 else if (age >= 60) {
   fee = 800 * 0.85;
   console.log("Senior citizens fare : ", fee, "tk");
-} else {
+} 
+else {
   console.log("Regular Ticket fare : 800tk");
 }
